aa438fc6580b931b3f00fd82400e03e700630026
Omar400-cpu <omardarhous123456@gmail.com>
2026-01-11 15:57:32 +0200

    Frontend
    Home and Authentication page

---
+++ Code/JS/Authentication.js
@@ -0,0 +1,44 @@
+const loginForm = document.getElementById("login-form");
+const signupForm = document.getElementById("signup-form");
+const tabs = document.querySelectorAll(".tab");
+
+function showLogin() {
+  loginForm.classList.remove("hidden");
+  signupForm.classList.add("hidden");
+  tabs[0].classList.add("active");
+  tabs[1].classList.remove("active");
+}
+
+function showSignup() {
+  signupForm.classList.remove("hidden");
+  loginForm.classList.add("hidden");
+  tabs[1].classList.add("active");
+  tabs[0].classList.remove("active");
+}
+
+/* PASSWORD TOGGLE */
+function togglePassword(inputId, eye) {
+  const input = document.getElementById(inputId);
+  const icon = eye.querySelector("i");
+
+  if (input.type === "password") {
+    // SHOW password
+    input.type = "text";
+    icon.classList.remove("fa-eye");
+    icon.classList.add("fa-eye-slash");
+  } else {
+    // HIDE password
+    input.type = "password";
+    icon.classList.remove("fa-eye-slash");
+    icon.classList.add("fa-eye");
+  }
+}
+
+
+
+/* AUTO SWITCH FROM URL */
+document.addEventListener("DOMContentLoaded", () => {
+  const params = new URLSearchParams(window.location.search);
+  const mode = params.get("mode");
+  mode === "signup" ? showSignup() : showLogin();
+});
